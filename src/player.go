6cdb5e1474f3d9dea12860d8bbf3b46b47c3c52c
John Beisley <greatred@gmail.com>
2010-12-18 15:45:59 +0000

    Added a TODO comment regarding player visibility tracking.

--- src/player.go
+++ src/player.go
@@ -61,6 +61,12 @@ func (player *Player) PacketPlayerPosition(position *XYZ, stance AbsoluteCoord, 
     log.Printf("PacketPlayerPosition position=(%.2f, %.2f, %.2f) stance=%.2f flying=%v",
         position.x, position.y, position.z, stance, flying)
 
+    // TODO: Should keep track of when players enter/leave their mutual radius
+    // of "awareness". I.e a client should receive a RemoveEntity packet when
+    // the player walks out of range, and no longer receive WriteEntityTeleport
+    // packets for them. The converse should happen when players come in range
+    // of each other.
+
     player.game.Enqueue(func(game *Game) {
         var delta = XYZ{position.x - player.position.x,
             position.y - player.position.y,
