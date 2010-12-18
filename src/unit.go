3fab341850857e75af757903a1fd8e274b811bf3
John Beisley <greatred@gmail.com>
2010-12-18 16:50:20 +0000

    Corrected some type comments, and added a block->chunk coord fn.

--- src/unit.go
+++ src/unit.go
@@ -16,11 +16,11 @@ type Face byte
 // Specifies exact world location in pixels
 type AbsoluteCoord float64
 
-// Specifies approximate world coordinate in pixels (same as AbsoluteCoord, but
-// with only integer precision).
+// Specifies approximate world coordinate in pixels (absolute / 32 ?)
+// TODO verify the physical size of values of this type
 type AbsoluteCoordInteger int32
 
-// Coordinate of a block within the world (absolute / 32)
+// Coordinate of a block within the world (integer version of AbsoluteCoord)
 type BlockCoord int32
 
 // Coordinate of a chunk in the world (block / 16)
@@ -32,4 +32,9 @@ type AngleRadians float32
 // Convert an (x, z) absolute coordinate pair to chunk coordinates
 func AbsoluteToChunkCoords(absX AbsoluteCoord, absZ AbsoluteCoord) (chunkX ChunkCoord, chunkZ ChunkCoord) {
     return ChunkCoord(absX / ChunkSizeX), ChunkCoord(absZ / ChunkSizeZ)
+}
+
+// Convert an (x, z) block coordinate pair to chunk coordinates
+func BlockToChunkCoords(blockX AbsoluteCoord, blockZ AbsoluteCoord) (chunkX ChunkCoord, chunkZ ChunkCoord) {
+    return ChunkCoord(blockX / ChunkSizeX), ChunkCoord(blockZ / ChunkSizeZ)
 }
