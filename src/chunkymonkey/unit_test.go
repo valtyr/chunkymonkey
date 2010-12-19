bcaf248283dd0259b74367940ed53a4cfdbc6bff
John Beisley <greatred@gmail.com>
2010-12-19 13:39:12 +0000

    Fixed and tidied unit test.

--- src/chunkymonkey/unit_test.go
+++ src/chunkymonkey/unit_test.go
@@ -4,25 +4,32 @@ import (
     "testing"
 )
 
-func testCoordDivModCase(t *testing.T, expected_div, expected_mod, num, denom int32) {
-    div, mod := coordDivMod(num, denom)
-    if expected_div != div || expected_mod != mod {
-        t.Errorf("coordDivMod(%d, %d) expected (%d, %d) got (%d, %d)",
-            0, 16, expected_div, expected_mod, div, mod)
-    }
+type CoordDivModTest struct {
+    expected_div, expected_mod int32
+    num, denom int32
+}
+
+var CoordDivModTests = []CoordDivModTest{
+    // Simple +ve numerator cases
+    CoordDivModTest{0, 0, 0, 16},
+    CoordDivModTest{0, 1, 1, 16},
+    CoordDivModTest{0, 15, 15, 16},
+    CoordDivModTest{1, 0, 16, 16},
+    CoordDivModTest{1, 15, 31, 16},
+
+    // -ve numerator cases
+    CoordDivModTest{-1, 15, -1, 16},
+    CoordDivModTest{-1, 0, -16, 16},
+    CoordDivModTest{-2, 15, -17, 16},
+    CoordDivModTest{-2, 0, -32, 16},
 }
 
 func TestCoordDivMod(t *testing.T) {
-    // Simple +ve numerator cases
-    testCoordDivModCase(t, 0, 0, 0, 16)
-    testCoordDivModCase(t, 0, 1, 1, 16)
-    testCoordDivModCase(t, 0, 15, 15, 16)
-    testCoordDivModCase(t, 1, 0, 16, 16)
-    testCoordDivModCase(t, 1, 15, 31, 16)
-
-    // -ve numerator cases
-    testCoordDivModCase(t, -1, 15, -1, 16)
-    testCoordDivModCase(t, -1, 0, -16, 16)
-    testCoordDivModCase(t, -2, 15, -17, 16)
-    testCoordDivModCase(t, -2, 0, -32, 16)
+    for _, r := range CoordDivModTests {
+        div, mod := coordDivMod(r.num, r.denom)
+        if r.expected_div != div || r.expected_mod != mod {
+            t.Errorf("coordDivMod(%d, %d) expected (%d, %d) got (%d, %d)",
+                r.num, r.denom, r.expected_div, r.expected_mod, div, mod)
+        }
+    }
 }
