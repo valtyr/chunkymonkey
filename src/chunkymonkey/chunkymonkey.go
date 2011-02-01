34d7a57714b824358a9b27803f6bb3f41bbcb2cf
John Beisley <greatred@gmail.com>
2011-02-01 23:10:12 +0000

    Adding HTTP server instrumentation for chunkymonkey server.

--- src/chunkymonkey/chunkymonkey.go
+++ src/chunkymonkey/chunkymonkey.go
@@ -1,19 +1,41 @@
 package main
 
 import (
-    "chunkymonkey/chunkymonkey"
+    _ "expvar"
     "flag"
+    "http"
+    "log"
+    "net"
     "os"
+
+    "chunkymonkey/chunkymonkey"
 )
 
-var addr = flag.String("addr", ":25565", "Serves on the given address:port.")
+var addr = flag.String(
+    "addr", ":25565",
+    "Serves on the given address:port.")
+
+var httpAddr = flag.String(
+    "http_addr", ":25566",
+    "Serves HTTP diagnostics on the given address:port.")
 
 func usage() {
     os.Stderr.WriteString("usage: " + os.Args[0] + " [flags] <world>\n")
     flag.PrintDefaults()
 }
 
+func startHttpServer(addr string) (err os.Error) {
+    httpPort, err := net.Listen("tcp", addr)
+    if err != nil {
+        return
+    }
+    go http.Serve(httpPort, nil)
+    return
+}
+
 func main() {
+    var err os.Error
+
     flag.Usage = usage
     flag.Parse()
 
@@ -23,7 +45,12 @@ func main() {
     }
 
     worldPath := flag.Arg(0)
-
     game := chunkymonkey.NewGame(worldPath)
+
+    err = startHttpServer(*httpAddr)
+    if err != nil {
+        log.Panic(err)
+    }
+
     game.Serve(*addr)
 }
