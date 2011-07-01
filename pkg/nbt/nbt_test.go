b79b8cdee240b25b177ff00247d6c078bf2467b6
John Beisley <greatred@gmail.com>
2011-07-01 21:45:02 +0100

    Added more comprehensive unit tests for the nbt package.

--- pkg/nbt/nbt_test.go
+++ pkg/nbt/nbt_test.go
@@ -4,22 +4,27 @@ import (
 	"bytes"
 	"fmt"
 	"reflect"
-	"strings"
 	"testing"
+
+	te "testencoding"
 )
 
+
 type Test struct {
-	serialized string
-	value      ITag
+	Serialized te.BytesMatcher
+	Value      ITag
 }
 
 func (test *Test) String() string {
-	return fmt.Sprintf("Test{serialized=%q, value=%#v}", test.serialized, test.value)
+	return fmt.Sprintf("Test{Serialized=%v, Value=%#v}", test.Serialized, test.Value)
 }
 
-func (test *Test) testRead(t *testing.T) {
-	result := NewTagByType(test.value.GetType())
-	err := result.Read(strings.NewReader(test.serialized))
+func (test *Test) testRead(t *testing.T, ) {
+	bytesBuf := new(bytes.Buffer)
+	test.Serialized.Write(bytesBuf)
+
+	result := NewTagByType(test.Value.GetType())
+	err := result.Read(bytesBuf)
 
 	t.Logf("Test read %v", test)
 
@@ -28,7 +33,7 @@ func (test *Test) testRead(t *testing.T) {
 		return
 	}
 
-	if !reflect.DeepEqual(test.value, result) {
+	if !reflect.DeepEqual(test.Value, result) {
 		t.Errorf("  Fail: got result = %T%v", result, result)
 	}
 }
@@ -37,14 +42,16 @@ func (test *Test) testWrite(t *testing.T) {
 	t.Logf("Test write %v", test)
 
 	resultBuf := new(bytes.Buffer)
-	err := test.value.Write(resultBuf)
+	err := test.Value.Write(resultBuf)
 	if err != nil {
-		t.Errorf("  Fail: failed to write %#v: %T%v", test.value, err, err)
+		t.Errorf("  Fail: failed to write %#v: %T%v", test.Value, err, err)
 		return
 	}
 
 	result := resultBuf.Bytes()
-	if !reflect.DeepEqual([]byte(test.serialized), result) {
+	n, matches := test.Serialized.Match(result)
+
+	if !matches || n != len(result) {
 		t.Errorf("  Fail: got result = %T%v", result, result)
 	}
 }
@@ -52,23 +59,63 @@ func (test *Test) testWrite(t *testing.T) {
 
 func TestSerialization(t *testing.T) {
 	tests := []Test{
-		{"", &End{}},
-		{"\x01", &Byte{1}},
-		{"\x10\x20", &Short{0x1020}},
-		{"\x10\x20\x30\x40", &Int{0x10203040}},
-		{"\x10\x20\x30\x40\x50\x60\x70\x80", &Long{0x1020304050607080}},
-		{"\x3f\x80\x00\x00", &Float{1.0}},
-		{"\x3f\xf0\x00\x00\x00\x00\x00\x00", &Double{1.0}},
-		{"\x00\x00\x00\x04\x00\x01\x02\x03", &ByteArray{[]byte{0, 1, 2, 3}}},
-		{"\x00\x03foo", &String{"foo"}},
-		{"\x01\x00\x00\x00\x02\x01\x02", &List{TagByte, []ITag{&Byte{1}, &Byte{2}}}},
-		{"\x03\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02", &List{TagInt, []ITag{&Int{1}, &Int{2}}}},
+		{te.LiteralString(""), &End{}},
+		{te.LiteralString("\x01"), &Byte{1}},
+		{te.LiteralString("\x10\x20"), &Short{0x1020}},
+		{te.LiteralString("\x10\x20\x30\x40"), &Int{0x10203040}},
+		{te.LiteralString("\x10\x20\x30\x40\x50\x60\x70\x80"), &Long{0x1020304050607080}},
+		{te.LiteralString("\x3f\x80\x00\x00"), &Float{1.0}},
+		{te.LiteralString("\x3f\xf0\x00\x00\x00\x00\x00\x00"), &Double{1.0}},
+		{te.LiteralString("\x00\x00\x00\x04\x00\x01\x02\x03"), &ByteArray{[]byte{0, 1, 2, 3}}},
+		{te.LiteralString("\x00\x03foo"), &String{"foo"}},
+		{te.LiteralString("\x01\x00\x00\x00\x02\x01\x02"), &List{TagByte, []ITag{&Byte{1}, &Byte{2}}}},
+		{te.LiteralString("\x03\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02"), &List{TagInt, []ITag{&Int{1}, &Int{2}}}},
+
 		{
-			"\x01\x00\x03foo\x01" + // NamedTag "foo" Byte{1}
-				"\x00", // End
+			// Single item Compound.
+			te.InOrder(
+				te.LiteralString("\x01\x00\x03foo\x01"), // NamedTag "foo" Byte{1}
+				te.LiteralString("\x00"), // End
+			),
 			&Compound{
 				map[string]*NamedTag{
 					"foo": &NamedTag{"foo", &Byte{1}},
+				},
+			},
+		},
+		{
+			// Multiple item Compound.
+			te.InOrder(
+				te.AnyOrder(
+					// NamedTag "Byte" Byte{1}
+					te.LiteralString("\x01\x00\x04Byte\x01"),
+					// NamedTag "Short" Short{2}
+					te.LiteralString("\x02\x00\x05Short\x00\x02"),
+					// NamedTag "Int" Int{3}
+					te.LiteralString("\x03\x00\x03Int\x00\x00\x00\x03"),
+					// NamedTag "Long" Long{4}
+					te.LiteralString("\x04\x00\x04Long\x00\x00\x00\x00\x00\x00\x00\x04"),
+					// NamedTag "Float" Float{5}
+					te.LiteralString("\x05\x00\x05Float\x40\xa0\x00\x00"),
+					// NamedTag "Double" Double{6}
+					te.LiteralString("\x06\x00\x06Double\x40\x18\x00\x00\x00\x00\x00\x00"),
+					// NamedTag "String" String{"foo"}
+					te.LiteralString("\x08\x00\x06String\x00\x03foo"),
+					// NamedTag "List" List{Byte{1}, Byte{2}}
+					te.LiteralString("\x09\x00\x04List\x01\x00\x00\x00\x02\x01\x02"),
+				),
+				te.LiteralString("\x00"), // End
+			),
+			&Compound{
+				map[string]*NamedTag{
+					"Byte": &NamedTag{"Byte", &Byte{1}},
+					"Short": &NamedTag{"Short", &Short{2}},
+					"Int": &NamedTag{"Int", &Int{3}},
+					"Long": &NamedTag{"Long", &Long{4}},
+					"Float": &NamedTag{"Float", &Float{5}},
+					"Double": &NamedTag{"Double", &Double{6}},
+					"String": &NamedTag{"String", &String{"foo"}},
+					"List": &NamedTag{"List", &List{TagByte, []ITag{&Byte{1}, &Byte{2}}}},
 				},
 			},
 		},
