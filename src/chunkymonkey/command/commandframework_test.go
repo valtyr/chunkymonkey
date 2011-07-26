6fc86127b00fb6e62be7f874b4a3ecf772947a3c
James Whitehead II <jnwhiteh@gmail.com>
2011-07-27 00:02:10 +0100

    Tests compile, but are not all passing yet

--- src/chunkymonkey/command/commandframework_test.go
+++ src/chunkymonkey/command/commandframework_test.go
@@ -14,39 +14,41 @@ func TestCommandFramework(t *testing.T) {
 	defer mockCtrl.Finish()
 
 	mockGame := gamerules.NewMockIGame(mockCtrl)
+	mockPlayer := gamerules.NewMockIPlayerClient(mockCtrl)
+	mockOther := gamerules.NewMockIPlayerClient(mockCtrl)
 
 	cf := NewCommandFramework("/")
 
 	mockGame.EXPECT().BroadcastMessage("§dthis is a broadcast")
-	cf.Process("thePlayer", "/say this is a broadcast", mockGame)
+	cf.Process(mockPlayer, "/say this is a broadcast", mockGame)
 
-	mockGame.EXPECT().IsValidPlayerName("thePlayer").Return(true)
-	mockGame.EXPECT().IsValidItemId(1).Return(true)
-	mockGame.EXPECT().SendMessageToPlayer("thePlayer", "Giving 64 of 1 to thePlayer")
-	mockGame.EXPECT().GiveItem("thePlayer", 1, 64, 0)
-	cf.Process("thePlayer", "/give thePlayer 1 64", mockGame)
+	mockGame.EXPECT().PlayerByName("thePlayer").Return(mockPlayer)
+	mockGame.EXPECT().ItemTypeById(1)
+	mockPlayer.EXPECT().EchoMessage("Giving 64 of 1 to thePlayer")
+	mockPlayer.EXPECT().GiveItemAtPosition("thePlayer", gamerules.Slot{1, 64, 0})
+	cf.Process(mockPlayer, "/give thePlayer 1 64", mockGame)
 
-	mockGame.EXPECT().IsValidPlayerName("otherPlayer").Return(false)
-	mockGame.EXPECT().SendMessageToPlayer("thePlayer", "'otherPlayer' is not logged in")
-	cf.Process("thePlayer", "/give otherPlayer 1 64", mockGame)
+	mockGame.EXPECT().PlayerByName("otherPlayer")
+	mockPlayer.EXPECT().EchoMessage("'otherPlayer' is not logged in")
+	cf.Process(mockPlayer, "/give otherPlayer 1 64", mockGame)
 
-	mockGame.EXPECT().IsValidPlayerName("otherPlayer").Return(true)
-	mockGame.EXPECT().IsValidItemId(1).Return(false)
-	mockGame.EXPECT().SendMessageToPlayer("thePlayer", "'1' is not a valid item id")
-	cf.Process("thePlayer", "/give otherPlayer 1 64", mockGame)
+	mockGame.EXPECT().PlayerByName("otherPlayer").Return(mockOther)
+	mockGame.EXPECT().ItemTypeById(1).Return(gamerules.ItemType{}, false)
+	mockPlayer.EXPECT().EchoMessage("'1' is not a valid item id")
+	cf.Process(mockPlayer, "/give otherPlayer 1 64", mockGame)
 
-	mockGame.EXPECT().IsValidPlayerName("otherPlayer").Return(true)
-	mockGame.EXPECT().IsValidItemId(1).Return(true)
-	mockGame.EXPECT().SendMessageToPlayer("thePlayer", "Cannot give more than 512 items at once")
-	cf.Process("thePlayer", "/give otherPlayer 1 513", mockGame)
+	mockGame.EXPECT().PlayerByName("otherPlayer").Return(mockOther)
+	mockGame.EXPECT().ItemTypeById(1)
+	mockPlayer.EXPECT().EchoMessage("Cannot give more than 512 items at once")
+	cf.Process(mockPlayer, "/give otherPlayer 1 513", mockGame)
 
-	mockGame.EXPECT().SendMessageToPlayer("thePlayer", &testmatcher.StringPrefix{"Commands:"})
-	cf.Process("thePlayer", "/help", mockGame)
+	mockPlayer.EXPECT().EchoMessage(&testmatcher.StringPrefix{"Commands:"})
+	cf.Process(mockPlayer, "/help", mockGame)
 
 	gomock.InOrder(
-		mockGame.EXPECT().SendMessageToPlayer("thePlayer", "Command: /help"),
-		mockGame.EXPECT().SendMessageToPlayer("thePlayer", "Usage: help|?"),
-		mockGame.EXPECT().SendMessageToPlayer("thePlayer", "Description: Shows a list of all commands."),
+		mockPlayer.EXPECT().EchoMessage("Command: /help"),
+		mockPlayer.EXPECT().EchoMessage("Usage: help|?"),
+		mockPlayer.EXPECT().EchoMessage("Description: Shows a list of all commands."),
 	)
-	cf.Process("thePlayer", "/help help", mockGame)
+	cf.Process(mockPlayer, "/help help", mockGame)
 }
