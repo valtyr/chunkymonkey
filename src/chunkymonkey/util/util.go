c75b101cad4f64f5d5be75977319024583d67c04
John Beisley <greatred@gmail.com>
2011-08-09 08:26:51 +0100

    Added function to create a uniquely named file.

--- src/chunkymonkey/util/util.go
+++ src/chunkymonkey/util/util.go
@@ -2,7 +2,12 @@ package util
 
 import (
 	"os"
+	"rand"
+	"strconv"
+	"time"
 )
+
+var rdRand <-chan int64
 
 func Errno(err os.Error) (errno os.Errno, ok bool) {
 	if e, ok := err.(*os.PathError); ok {
@@ -10,4 +15,36 @@ func Errno(err os.Error) (errno os.Errno, ok bool) {
 	}
 	errno, ok = err.(os.Errno)
 	return
+}
+
+// OpenFileUniqueName creates a file with a unique (and randomly generated)
+// filename with the given path and name prefix. It is opened with
+// flag|os.O_CREATE|os.O_EXCL; os.O_WRONLY or os.RDWR should be specified for
+// flag at minimum. It is the caller's responsibility to close (and maybe
+// delete) the file when they have finished using it.
+func OpenFileUniqueName(prefix string, flag int, perm uint32) (file *os.File, err os.Error) {
+	useFlag := flag|os.O_CREATE|os.O_EXCL
+	for i := 0; i < 1000; i++ {
+		rnd := <-rdRand
+		if file, err := os.OpenFile(prefix + strconv.Itob64(rnd, 16), useFlag, perm); err == nil {
+			return file, err
+		} else {
+			if errno, ok := Errno(err); ok && errno == os.EEXIST {
+				continue
+			}
+			return nil, err
+		}
+	}
+	return nil, os.NewError("gave up trying to create unique filename")
+}
+
+func init() {
+	randChan := make(chan int64, 10)
+	rdRand = randChan
+	go func() {
+		source := rand.NewSource(time.Nanoseconds())
+		for {
+			randChan<- source.Int63()
+		}
+	}()
 }
