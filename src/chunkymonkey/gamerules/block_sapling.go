5b7bd491ca3793b5461382aed80d221060f4061c
James Whitehead II <jnwhiteh@gmail.com>
2011-08-11 09:00:05 +0100

    Grow saplings into stumps at a reasonable rate (initial)

--- src/chunkymonkey/gamerules/block_sapling.go
+++ src/chunkymonkey/gamerules/block_sapling.go
@@ -2,6 +2,9 @@ package gamerules
 
 import (
 	"os"
+	"rand"
+	"time"
+	. "chunkymonkey/types"
 )
 
 // Behaviour of a sapling block, takes care of growing or dying depending on
@@ -24,7 +27,29 @@ func (aspect *SaplingAspect) Check() os.Error {
 	return nil
 }
 
+var rnd = rand.New(rand.NewSource(time.Nanoseconds()))
+
 func (aspect *SaplingAspect) Tick(instance *BlockInstance) bool {
-	// TODO: Need to find a way to bootstrap a tick
+	if rnd.Intn(1e4) >= 1e4-1 {
+		// Turn this block into a tree
+		return aspect.makeTree(instance)
+	}
+	return true
+}
+
+func (aspect *SaplingAspect) makeTree(instance *BlockInstance) bool {
+	loc := instance.SubLoc
+	minheight := 3
+	maxheight := 6
+	height := loc.Y + SubChunkCoord(minheight+rand.Intn(maxheight-minheight))
+
+	for y := loc.Y; y < height; y++ {
+		loc.Y = y
+		index, ok := loc.BlockIndex()
+		if !ok {
+			return false
+		}
+		instance.Chunk.SetBlockByIndex(index, BlockId(17), byte(0))
+	}
 	return true
 }
